ef368bcbe377028bbb983deb8893604a1521a244
Dakota Gallimore <gallimoredakota@gmail.com>
2017-08-27 02:24:26 -0400

    Add Storing in Index Functionality

---
+++ web-crawler.py
@@ -0,0 +1,62 @@
+def get_page(url):
+    try:
+		import urllib
+		return urllib.urlopen(url).read()
+	except:
+		return ""
+
+def get_next_target(page):
+    start_link = page.find('<a href=')
+    if start_link == -1: 
+        return None, 0
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    return url, end_quote
+
+def union(p,q):
+    for e in q:
+        if e not in p:
+            p.append(e)
+
+def get_all_links(page):
+    links = []
+    while True:
+        url,endpos = get_next_target(page)
+        if url:
+            links.append(url)
+            page = page[endpos:]
+        else:
+            break
+    return links
+
+def crawl_web(seed):
+    tocrawl = [seed]
+    crawled = []
+    while tocrawl:
+        page = tocrawl.pop()
+        if page not in crawled:
+            union(tocrawl, get_all_links(get_page(page)))
+            crawled.append(page)
+    return crawled
+	
+	
+# If the keyword is already
+# in the index, add the url
+# to the list of urls associated
+# with that keyword.
+
+# If the keyword is not in the index,
+# add an entry to the index: [keyword,[url]]
+
+index = []
+
+def add_to_index(index,keyword,url):
+    for x in index:
+        if x[0] == keyword:
+            x[1].append(url)
+            break
+    else:
+        url_list = [keyword, []]
+        url_list[1].append(url)
+        index.append(url_list)
